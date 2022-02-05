924050b1967d23941f455f495ef6de830591ebc6
Roman Hyrych <roman.girich@gmail.com>
2022-02-05 03:59:36 +0200

    added to_top button, icons and some styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './sass/main.scss';
 
 import * as bootstrap from 'bootstrap';
+import '../node_modules/@fortawesome/fontawesome-free/js/brands.min';
+import '../node_modules/@fortawesome/fontawesome-free/js/solid.min';
+import '../node_modules/@fortawesome/fontawesome-free/js/fontawesome.min';
 import './js/fetch_api';
 import './js/rendered_cards';
+import './js/to_top';
